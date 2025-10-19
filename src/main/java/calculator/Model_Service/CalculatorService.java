8f2826e28d00972b140d4a90ad158272ac33b4b5
clover9593 <clover9593@gmail.com>
2025-10-19 17:39:19 +0900

    feat: 오버플로우 처리, 여러개의 문자열을 받을때 "["와 "]" 제거하는 기능 구현

--- src/main/java/calculator/Model_Service/CalculatorService.java
+++ src/main/java/calculator/Model_Service/CalculatorService.java
@@ -3,7 +3,7 @@ package calculator.Model_Service;
 import java.util.regex.Pattern;
 
 public class CalculatorService {
-    public int calculate(String input) {
+    public long calculate(String input) {
         if (input == null || input.isEmpty()) {
             return 0;
         }
@@ -14,12 +14,17 @@ public class CalculatorService {
 
         // 입력된 문자열이 "//" 로 시작한다면 커스텀 구분자 추가 후 input을 "//?\n"가 없는 형태로 수정
         if (input.startsWith("//")) {
-            int newLineIndex = input.indexOf("\n");
-            if (newLineIndex == -1) {
+            String[] parts = input.split("\n|\\\\n", 2);
+            if (parts.length < 2) {
                 throw new IllegalArgumentException("커스텀 구분자 형식이 올바르지 않습니다. (\\n 없음)");
             }
 
-            String customDelimiter = input.substring(2, newLineIndex);
+            String customDelimiter = parts[0].substring(2);
+            input = parts[1];
+
+            if (customDelimiter.startsWith("[") && customDelimiter.endsWith("]")) {
+                customDelimiter = customDelimiter.substring(1, customDelimiter.length() - 1);
+            }
 
             if (customDelimiter.equals("-")) {
                 throw new IllegalArgumentException("-는 구분자로 받을 수 없습니다.");
@@ -33,19 +38,25 @@ public class CalculatorService {
             }
 
             deli += "|" + Pattern.quote(customDelimiter);
-
-            input = input.substring(newLineIndex + 1);
-
         }
 
         String[] num_list = input.split(deli);
 
-        int sum = 0;
+        long sum = 0;
         for (int i = 0; i < num_list.length; i++) {
-            if (Integer.parseInt(num_list[i]) <= 0) {
-                throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
-            } else {
-                sum += Integer.parseInt(num_list[i]);
+            if (num_list[i].isEmpty()) {
+                continue;
+            }
+
+            try {
+                if (Long.parseLong(num_list[i]) < 0) {
+                    throw new IllegalArgumentException("음수는 입력할 수 없습니다.");
+                } else {
+                    sum += Long.parseLong(num_list[i]);
+                }
+            } catch (NumberFormatException e) {
+                // 실수나 문자가 들어오면 예외처리
+                throw new IllegalArgumentException("유효하지 않은 숫자 형식입니다.", e);
             }
         }
 
